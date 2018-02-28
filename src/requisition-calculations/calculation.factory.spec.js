e0b5822d4b4ec71edab57463a3a1dcc64162f249
Nikodem Graczewski <ngraczewski@soldevelo.com>
2018-02-28 11:29:10 +0100

    OLMIS-4101: Fixed test name

--- src/requisition-calculations/calculation.factory.spec.js
+++ src/requisition-calculations/calculation.factory.spec.js
@@ -219,7 +219,7 @@ describe('calculationFactory', function() {
             expect(calculationFactory.packsToShip(lineItem, requisitionMock)).toBe(0);
         });
 
-        it('should always use requested quantity column if requistiion is emergency', function() {
+        it('should always use requested quantity column if requisition is emergency', function() {
             requisitionMock.emergency = true;
 
             lineItem.requestedQuantity = undefined;
