7ccbf1de96190ab7839e9fccb63a7fc062dc28ac
Nikodem Graczewski <ngraczewski@soldevelo.com>
2018-10-17 10:17:32 +0200

    Restored old version of the should save requisition test

--- src/requisition/requisition.spec.js
+++ src/requisition/requisition.spec.js
@@ -72,7 +72,6 @@ describe('Requisition', function() {
             OrderableDataBuilder = $injector.get('OrderableDataBuilder');
             LineItem = $injector.get('LineItem');
             UuidGenerator = $injector.get('UuidGenerator');
-            this.dateUtils = $injector.get('dateUtils');
         });
 
         var requisitionDataBuilder = new RequisitionDataBuilder();
@@ -440,48 +439,10 @@ describe('Requisition', function() {
         it('should save requisition', function() {
             var data;
 
-            var expected = angular.copy(requisition);
-
-            expected.requisitionLineItems[0].stockOnHand = null;
-            expected.requisitionLineItems[1].stockOnHand = null;
-            expected.requisitionLineItems[2].stockOnHand = null;
-
-            delete requisition.requisitionLineItems[0].$program;
-            delete requisition.requisitionLineItems[1].$program;
-            delete requisition.requisitionLineItems[2].$program;
-
-            delete requisition.requisitionLineItems[0].$errors;
-            delete requisition.requisitionLineItems[1].$errors;
-            delete requisition.requisitionLineItems[2].$errors;
-
-            expected.requisitionLineItems[0].orderable = {
-                id: expected.requisitionLineItems[0].orderable.id
-            };
-            expected.requisitionLineItems[1].orderable = {
-                id: expected.requisitionLineItems[1].orderable.id
-            };
-            expected.requisitionLineItems[2].orderable = {
-                id: expected.requisitionLineItems[2].orderable.id
-            };
-
-            expected.processingPeriod.startDate = '2017-01-01';
-            expected.processingPeriod.endDate = '2017-01-31';
-
-            expected.program = {
-                id: expected.program.id
-            };
-
-            expected.facility = {
-                id: expected.facility.id
-            };
-
-            delete expected.availableNonFullSupplyProducts;
-            delete expected.availableFullSupplyProducts;
-            delete expected.stockAdjustmentReasons;
-            delete expected.template;
-
-            $httpBackend.when('PUT', requisitionUrlFactory('/api/requisitions/' + requisition.id), expected)
+            $httpBackend.when('PUT', requisitionUrlFactory('/api/requisitions/' + requisition.id))
                 .respond(200, requisition);
+
+            requisition.name = 'Saved requisition';
 
             requisition.$save().then(function(response) {
                 data = response;
